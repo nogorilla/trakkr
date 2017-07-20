2842e11bc7fb9f6fab147526b431edb99a882227
gabe <gabriel.smith01@gmail.com>
2017-07-20 16:48:52 -0500

    Display current month trackr on homepage

--- controllers/home.js
+++ controllers/home.js
@@ -1,15 +1,28 @@
-/**
- * GET /
- * Home page.
- */
+const Trackr = require('../models/Trackr');
+const moment = require('moment');
+
 exports.index = (req, res) => {
-  if (!req.user) {
-    res.render('landing', {
-      title: 'Welcome'
+  let view  = 'landing'
+      title = 'Welcome',
+      trackrs = null;
+
+  if (req.user) {
+    view = 'home';
+    title = 'Trackr';
+    Trackr.find({created_by: req.user.id}, (err, trackrs) => {
+      res.render(view, {
+        trackrs: trackrs.reduce((trackrMap, trackr) => {
+          if (moment(trackr.date).get('month') === moment().get('month')) {
+            trackrMap[trackr.id] = trackr;
+          }
+          return trackrMap;
+        }, {}),
+        'title': title
+      });
     });
   } else {
-    res.render('home', {
-      title: 'Trackr'
+    res.render('landing', {
+      title: 'Welcome'
     });
   }
 };
