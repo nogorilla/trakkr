faccfb1e1e5c9d337c5311003ad59bdd773180be
Gabriel <gabriel.smith01@gmail.com>
2017-12-31 17:02:42 -0500

    fix spelling error

--- src/app.js
+++ src/app.js
@@ -50,7 +50,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
 mongoose.connection.on('error', (err) => {
   console.error(err);
-  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chaclk.red('✗'));
+  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
   process.exit();
 });
 
